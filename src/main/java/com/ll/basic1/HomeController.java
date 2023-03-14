acdeb15fb0d6721b69233273465cef41e0a85ee7
Genie16-github <4411dlwoqja@naver.com>
2023-03-14 11:54:56 +0900

    increase, plus 추가

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -2,10 +2,17 @@ package com.ll.basic1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+    private int i;
+
+    public HomeController() {
+        i = 0;
+    }
+
     @GetMapping("/home/main")
     @ResponseBody
     public String showMain(){
@@ -21,6 +28,18 @@ public class HomeController {
     @GetMapping("/home/main3")
     @ResponseBody
     public String showMain3(){
-        return "즐거웠습니다.";
+        return "즐거웠습니다!";
+    }
+
+    @GetMapping("/home/increase")
+    @ResponseBody
+    public int showIncrease(){
+        return i++;
+    }
+
+    @GetMapping("/home/plus")
+    @ResponseBody
+    public int showPlus(@RequestParam(defaultValue = "0") int a, int b){
+        return a + b;
     }
 }
