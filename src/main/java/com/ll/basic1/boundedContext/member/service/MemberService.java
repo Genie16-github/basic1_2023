db7f7d3b597adc829121716183586d617bee1a65
Genie16-github <4411dlwoqja@naver.com>
2023-03-15 15:05:52 +0900

    내정보, 로그아웃 기능 구현

--- src/main/java/com/ll/basic1/boundedContext/member/service/MemberService.java
+++ src/main/java/com/ll/basic1/boundedContext/member/service/MemberService.java
@@ -3,18 +3,14 @@ package com.ll.basic1.boundedContext.member.service;
 import com.ll.basic1.base.RsData;
 import com.ll.basic1.boundedContext.member.entity.Member;
 import com.ll.basic1.boundedContext.member.repository.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 
 @Service
+@AllArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
-
-    @Autowired
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
 
     public RsData tryLogin(String username, String password) {
         Member member = memberRepository.findByUsername(username);
@@ -27,6 +23,10 @@ public class MemberService {
             return RsData.of("F-1", "비밀번호가 일치하지 않습니다.");
         }
 
-        return RsData.of("S-1", "%s 님 환영합니다.".formatted(username));
+        return RsData.of("S-1", "%s 님 환영합니다.".formatted(username), member.getId());
+    }
+
+    public Member findById(long id) {
+        return memberRepository.findById(id);
     }
 }
