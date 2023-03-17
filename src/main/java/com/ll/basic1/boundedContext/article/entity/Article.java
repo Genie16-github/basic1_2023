86db3a74b1e9fcb8233ce411ee0711c6306471fd
Genie16-github <4411dlwoqja@naver.com>
2023-03-17 11:09:56 +0900

    AUTO_INCREMENT

--- src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
+++ src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
@@ -1,12 +1,21 @@
 package com.ll.basic1.boundedContext.article.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 public class Article {
     @Id
+    @GeneratedValue(strategy = IDENTITY)  // AUTO_INCREMENT
     private long id;
     private String title;
     private String body;
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
+
 }
